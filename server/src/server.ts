1332dcc4d3c11b8b43c60a25a99db18048472852
Alexandre Santos <sasle57@gmail.com>
2020-06-02 18:38:30 -0300

    Adding Database
    Using SQLite and Knex.js

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,37 +1,8 @@
 import express, { request, response } from 'express';
-
+import routes from './routes'
 const app = express();
 
-const users  = [
-    'Diego',
-    'Alexandre',
-    'Artur',
-    'José'
-]
-
-app.get("/users", (request, response) => {
-    const search = String(request.query.search);
-
-    const filteredUsers = search ? users.filter(user => user.includes(search)) : users;
-
-    return response.json(filteredUsers);
-})
-
-app.get('/users/:id', (request, response) => {
-    const id = Number(request.params.id)
-
-    const user = users[id]
-
-    return response.json(user);
-})
-
-app.post('/users', (request, response) => {
-    const user = {
-        name: 'Alexandre',
-        email: 'ale@ale.com.br'
-    }
-
-    return response.json(user);
-})
+app.use(express.json())
+app.use(routes)
 
 app.listen(3333);
