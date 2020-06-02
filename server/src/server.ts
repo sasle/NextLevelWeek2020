7f23fb628b82676395016fe03e2ff3196fec86ff
Alexandre Santos <sasle57@gmail.com>
2020-06-02 18:22:34 -0300

    Query Params & Request Params
    Learning and applying them

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,18 +1,37 @@
-import express from 'express';
+import express, { request, response } from 'express';
 
 const app = express();
 
+const users  = [
+    'Diego',
+    'Alexandre',
+    'Artur',
+    'José'
+]
+
 app.get("/users", (request, response) => {
-    console.log('Listagem de usuários');
+    const search = String(request.query.search);
 
-    //JSON
+    const filteredUsers = search ? users.filter(user => user.includes(search)) : users;
 
-    response.json([
-        'Diego',
-        'Alexandre',
-        'Artur',
-        'José'
-    ])
+    return response.json(filteredUsers);
+})
+
+app.get('/users/:id', (request, response) => {
+    const id = Number(request.params.id)
+
+    const user = users[id]
+
+    return response.json(user);
+})
+
+app.post('/users', (request, response) => {
+    const user = {
+        name: 'Alexandre',
+        email: 'ale@ale.com.br'
+    }
+
+    return response.json(user);
 })
 
 app.listen(3333);
