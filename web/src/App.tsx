c7ec577d5e29715c35b2c22e79806b48758e65a2
Alexandre Santos <sasle57@gmail.com>
2020-06-03 11:58:41 -0300

    Creating Header component

---
+++ web/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Header from './Header'
+
+
+function App() {
+  return (
+        <div>
+          <Header></Header>
+          <h1>conteúdo da aplicação</h1>
+        </div>
+  );
+}
+
+export default App;
